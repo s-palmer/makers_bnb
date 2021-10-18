265e50133204925e19ad2c32170dc39661b8c00b
Max Fry <maxcfry@outlook.com>
2021-10-18 15:43:51 +0100

    basic rspec test

--- app.rb
+++ app.rb
@@ -7,18 +7,30 @@ require_relative 'database_connection_setup'
 
 class MakersBNB < Sinatra::Base
 
-
   configure :development do
     register Sinatra::Reloader
     register Sinatra::Flash
     register Sinatra::Partial
   end
 
-
   enable :sessions, :method_override, :partial_underscores
   set :partial_template_engine, :erb
 
+  get ('/') do 
+    "Welcome to MakersBnb"
+  end
 
+  get ('/login') do 
+
+  end
+
+  post ('/login') do 
+
+  end
+
+  get ('/requests') do
+
+  end
 
   run! if app_file == $PROGRAM_NAME
 end
