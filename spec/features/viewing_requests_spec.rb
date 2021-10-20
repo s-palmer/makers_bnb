aef454c576716e67ce09e09e677aa7636f7674af
Douglas <douglaskerins@gmail.com>
2021-10-20 13:50:36 +0100

    add buttons to requests page

--- spec/features/viewing_requests_spec.rb
+++ spec/features/viewing_requests_spec.rb
@@ -78,5 +78,31 @@ feature 'Viewing Requests' do
     expect(page).not_to have_content("Requests I've received:")
     expect(page).to have_content("Please login to view this page.")
   end
+
+  scenario 'view confirm and deny buttons for incoming requests' do
+    host = User.create(name: 'Test Name', email_address: 'test@example.com', password: 'password123')
+    user = User.create(name: 'Bob', email_address: 'test2@example.com', password: 'password123')
+    space = Space.create(
+          name: 'TestSpace',
+          description: 'A tranquil test space in test land.',
+          price: 100,
+          available_from: '2021-10-19 00:00:00',
+          available_to: '2021-10-31 00:00:00',
+          user_id: user.id
+        )
+
+    DatabaseConnection.query("INSERT INTO bookings (start_date, end_date, booking_confirmed, user_id, space_id, host_id) VALUES 
+      ('2021-11-18 15:44:02.776337', '2021-12-18 15:44:02.776337', false, #{user.id}, #{space.id}, #{host.id} );")
+
+    visit '/'
+    click_button 'Sign In'
+    fill_in(:email_address_si, with: 'test@example.com')
+    fill_in(:password_si, with: 'password123')
+    click_button('Enter')
+
+    visit '/requests'
+    expect(page).to have_content("Confirm Booking Request?")
+    expect(page).to have_content("Deny Booking Request?")
+  end
 end
 
