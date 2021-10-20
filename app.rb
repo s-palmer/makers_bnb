7895f414df8f089605d37130a89a7a4dec204bbe
Arthur Fincham <arthurfincham@me.com>
2021-10-20 10:35:15 +0100

    add views for individual space

--- app.rb
+++ app.rb
@@ -38,6 +38,11 @@ class MakersBNB < Sinatra::Base
     erb :'spaces/new'
   end
 
+  get '/spaces/:id/view' do
+    @space = Space.find(id: params[:id])
+    erb :'view_space'
+  end
+
   post '/new-space' do
     Space.create(name: params[:name], description: params[:description], price: params[:price],
                  available_from: params[:availablefrom_date], available_to: params[:availableto_date], user_id: @user.id)
