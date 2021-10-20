e878feb1b7343e53554b89c4db9508f10bb2a455
Douglas <douglaskerins@gmail.com>
2021-10-20 10:41:30 +0100

    adding my-spaces page

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -64,4 +64,45 @@ describe Space do
       expect(spaces.first.price).to eq '100'
     end
   end
+
+  describe '.mine' do
+    it 'returns a list of the spaces I have listed' do
+      connection = PG.connect(dbname: 'makers_bnb_test')
+      connection.exec("INSERT INTO users (id, name, email_address, password) VALUES (1, 'Test', 'test@example.com', 'password');")
+      DatabaseConnection.query("TRUNCATE users CASCADE")
+      user = User.create(name: 'Test Name', email_address: 'test1@example.com', password: 'password123')
+      host = User.create(name: 'Test Name Other', email_address: 'test2@example.com', password: 'password123')
+      space = Space.create(
+        name: 'TestSpace',
+        description: 'A tranquil test space in test land.',
+        price: 100,
+        available_from: '2021-10-19 00:00:00',
+        available_to: '2021-10-31 00:00:00',
+        user_id: 2
+      )
+      Space.create(
+        name: 'TestSpace2',
+        description: 'A tranquil test space in test land.',
+        price: 100,
+        available_from: '2021-10-19 00:00:00',
+        available_to: '2021-10-31 00:00:00',
+        user_id: 1
+      )
+      Space.create(
+        name: 'TestSpace3',
+        description: 'A tranquil test space in test land.',
+        price: 100,
+        available_from: '2021-10-19 00:00:00',
+        available_to: '2021-10-31 00:00:00',
+        user_id: 2
+      )
+      spaces = Space.mine(id: host.id)
+
+      expect(spaces.length).to eq 2
+      expect(spaces.first.id).to eq space.id
+      expect(spaces.first.name).to eq 'TestSpace'
+      expect(spaces.first.description).to eq 'A tranquil test space in test land.'
+      expect(spaces.first.price).to eq '100'
+    end
+  end
 end
