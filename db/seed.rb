55ef17d844ea8a6f623c8f4398b606bdf2a0f998
Sergei Palmer <zigpalmer@gmail.com>
2021-10-21 12:46:20 +0100

    Added seed file

---
+++ db/seed.rb
@@ -0,0 +1,57 @@
+require_relative "../lib/user"
+require_relative "../lib/space"
+
+DatabaseConnection.setup("makers_bnb_development")
+DatabaseConnection.query('TRUNCATE users, spaces, dates, bookings RESTART IDENTITY;', [])
+
+user1 = User.create(email_address: "user1@example.com", name: "User1", password: "userpassword")
+user2 = User.create(email_address: "user2@example.com", name: "User2", password: "userpassword")
+
+space1 = Space.create(name: "Mountain Retreat", description: "Relax in the mountains", price: "200", available_from: "2021-10-01", available_to: "2021-10-15", user_id: user1.id, url: "https://images.unsplash.com/photo-1558036117-15d82a90b9b1")
+
+BookingCalendar.create(
+  space_id: space1.id,
+  start_date: "2021-10-01",
+  end_date: "2021-10-15",
+)
+
+space2 = Space.create(name: "Ski Retreat", description: "Hit the slopes", price: "150", available_from: "2021-12-01", available_to: "2021-12-31", user_id: user1.id, url: "https://images.unsplash.com/photo-1550503736-c1a2c9033c03")
+
+BookingCalendar.create(
+  space_id: space2.id,
+  start_date: "2021-12-01",
+  end_date: "2021-12-31",
+)
+
+space3 = Space.create(name: "Beach Resort", description: "Perfect your tan", price: "300", available_from: "2021-07-01", available_to: "2021-08-30", user_id: user2.id, url: "https://images.unsplash.com/photo-1525967304942-5e269b2dab58")
+
+BookingCalendar.create(
+  space_id: space3.id,
+  start_date: "2021-07-01",
+  end_date: "2021-08-30",
+)
+
+space4 = Space.create(name: "City Break", description: "Discover bustling night markets", price: "150", available_from: "2021-02-01", available_to: "2021-08-30", user_id: user2.id, url: "https://images.unsplash.com/photo-1598935898639-81586f7d2129")
+
+BookingCalendar.create(
+  space_id: space4.id,
+  start_date: "2021-02-01",
+  end_date: "2021-08-30",
+)
+
+space5 = Space.create(name: "Forest Cabin Hideaway", description: "Get away from it all", price: "100", available_from: "2021-03-01", available_to: "2021-07-31", user_id: user2.id, url: "https://images.unsplash.com/photo-1568729937315-2ef5ee9cf4f2")
+
+BookingCalendar.create(
+  space_id: space5.id,
+  start_date: "2021-03-01",
+  end_date: "2021-07-31",
+)
+
+space6 = Space.create(name: "Desert Wellness Spa", description: "Fun for all the family", price: "100", available_from: "2021-06-01", available_to: "2021-11-30", user_id: user1.id, url: "https://images.unsplash.com/photo-1470010762743-1fa2363f65ca")
+
+BookingCalendar.create(
+  space_id: space6.id,
+  start_date: "2021-06-01",
+  end_date: "2021-11-30",
+)
+
